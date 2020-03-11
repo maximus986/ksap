43788b507a45dd95f4fbe11ce5c3421b70cb6d4a
Aleksandar Maksimovic <aleksandar.maksimovic@p3ds.net>
2020-03-11 15:29:45 +0100

    Implement 404 page

--- src/components/button.js
+++ src/components/button.js
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import styled from '@emotion/styled';
 import { jsx, useThemeUI } from 'theme-ui';
+import { Link } from 'gatsby';
 
 export const Button = ({ path, children }) => {
   const {
@@ -22,17 +23,18 @@ export const Button = ({ path, children }) => {
   );
 };
 
-const Btn = styled.a`
+const Btn = styled(Link)`
   font-size: 1.2rem;
   padding: 1.5rem 3.4rem;
   text-align: center;
   text-transform: uppercase;
   border-radius: 4px;
   color: #fff;
-  border: ${props => `2px solid ${props.colors.primary}`};
+  border: ${props => `2px solid ${props.colors.secondary}`};
+  text-decoration: none;
   &:hover {
     background: transparent;
-    border-color: ${props => props.colors.primary};
-    color: ${props => props.colors.primary};
+    border-color: ${props => props.colors.heading};
+    color: ${props => props.colors.heading};
   }
 `;
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,44 @@
-import React from 'react';
+/** @jsx jsx */
+import styled from '@emotion/styled';
+import { jsx, useThemeUI } from 'theme-ui';
+import { Button } from '../components/button';
+import { SectionContainer } from '../components/sectionContainer';
 import SEO from '../components/seo';
+import sectionImg from '../images/section-bg.png';
 
-const NotFoundPage = () => (
-  <>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
-);
+const NotFoundPage = () => {
+  const {
+    theme: { colors },
+  } = useThemeUI();
+  return (
+    <Background {...{ colors }}>
+      <SectionContainer sectionTitle="stranica ne postoji">
+        <SEO title="Stranica ne postoji" />
+        <SubTitle sx={{ color: 'heading', fontFamily: 'heading' }}>
+          Zahtevana stranica nije pronađena.
+        </SubTitle>
+        <Button path="/">
+          <span>Početna strana</span>
+        </Button>
+      </SectionContainer>
+    </Background>
+  );
+};
 
 export default NotFoundPage;
+
+const SubTitle = styled.h3`
+  font-size: 2rem;
+  margin-bottom: 7rem;
+`;
+
+const Background = styled.div`
+  background-image: url(${sectionImg});
+  background-color: ${props => props.colors.primary};
+  background-position: center bottom;
+  background-repeat: no-repeat;
+  height: 100vh;
+  @media (min-width: 1200px) {
+    background-position: top right;
+  }
+`;
