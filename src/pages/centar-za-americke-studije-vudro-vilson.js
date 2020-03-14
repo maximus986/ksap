219df637852f4b422c279c837007cfad24fb01e0
Aleksandar Maksimovic <aleksandar.maksimovic@p3ds.net>
2020-03-14 23:51:33 +0100

    Create new pages

---
+++ src/pages/centar-za-americke-studije-vudro-vilson.js
@@ -0,0 +1,96 @@
+/** @jsx jsx */
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS, MARKS } from '@contentful/rich-text-types';
+import styled from '@emotion/styled';
+import { graphql, useStaticQuery } from 'gatsby';
+import { jsx, useThemeUI } from 'theme-ui';
+import Layout from '../components/layout';
+import { SectionContainer } from '../components/sectionContainer';
+import SEO from '../components/seo';
+import { useSiteMetadata } from '../hooks/useSiteMetadata';
+
+const CentarZaAmerickeStudijeVudroVilson = () => {
+  const {
+    siteMetadata: { VudroVilson },
+  } = useSiteMetadata();
+
+  const {
+    theme: { colors },
+  } = useThemeUI();
+
+  const { center } = useStaticQuery(graphql`
+    {
+      center: allContentfulCenter {
+        edges {
+          node {
+            aboutCenter {
+              json
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const vudroVilsonContent = center.edges[1].node.aboutCenter.json;
+
+  const Bold = ({ children }) => <span className="bold">{children}</span>;
+  const Text = ({ children }) => <p className="align-center">{children}</p>;
+
+  const options = {
+    renderMark: {
+      [MARKS.BOLD]: text => <Bold>{text}</Bold>,
+    },
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+    },
+  };
+
+  return (
+    <Layout>
+      <SEO title={VudroVilson} />
+      <SectionContainer sectionTitle="o centru" sectionBgColor={colors.muted}>
+        <Container>
+          <Content sx={{ fontFamily: 'body', color: 'primary' }}>
+            {documentToReactComponents(vudroVilsonContent, options)}
+          </Content>
+        </Container>
+      </SectionContainer>
+    </Layout>
+  );
+};
+
+export default CentarZaAmerickeStudijeVudroVilson;
+
+const Content = styled.div`
+  padding: 0 1.6rem;
+  letter-spacing: 2px;
+  font-size: 2rem;
+  line-height: 3rem;
+  font-weight: 300;
+  @media (min-width: 1200px) {
+    padding: 0;
+    font-size: 2rem;
+    line-height: 3.5rem;
+    text-align: left;
+  }
+  @media (min-width: 1800px) {
+    font-size: 2.5rem;
+  }
+  h5 {
+    font-size: 3rem;
+  }
+`;
+
+const Container = styled.div`
+  @media (min-width: 1200px) {
+    max-width: 1140px;
+    margin: 0 auto;
+  }
+  @media (min-width: 1600px) {
+    max-width: 1410px;
+  }
+  @media (min-width: 1800px) {
+    max-width: 1420px;
+  }
+`;
