da14ef92990f75447dbf7cf073d8a064652f5ddc
Aleksandar Maksimovic <aleksandar.maksimovic@p3ds.net>
2020-03-14 20:32:05 +0100

    Update theme

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -32,7 +32,7 @@ export default {
     heading: 1.125,
   },
   fontSizes: [2, 4, 14, 16, 20, 24, 32, 48, 64, 72],
-  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  space: [0, 4, 8, 16, 24, 32, 64, 128, 256, 512],
   transition: {
     link: '0.3s ease',
   },
