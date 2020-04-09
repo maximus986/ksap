1ab6043d6d8283d925708fa52f440e09c844c8d3
Aleksandar Maksimovic <aleksandar.maksimovic@p3ds.net>
2020-04-09 11:18:12 +0200

    Improve section style and functionality

--- src/components/Academy.js
+++ src/components/Academy.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import Hero from './hero';
-import { graphql, useStaticQuery } from 'gatsby';
+/** @jsx jsx */
+import styled from '@emotion/styled';
+import { graphql, Link, useStaticQuery } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
+import { jsx } from 'theme-ui';
 
 export const Academy = () => {
   const { academyImage } = useStaticQuery(graphql`
     {
-      academyImage: file(relativePath: { eq: "sa-academy.png" }) {
+      academyImage: file(relativePath: { eq: "sa-academy.jpg" }) {
         name
         childImageSharp {
           fluid(maxWidth: 4160, quality: 90) {
@@ -17,6 +19,46 @@ export const Academy = () => {
   `);
 
   return (
-    <Hero img={academyImage.childImageSharp.fluid} alt={academyImage.name} />
+    <BgImage fluid={academyImage.childImageSharp.fluid} alt={academyImage.name}>
+      <h1 sx={{ textAlign: 'center', width: ['95%', '88%'], mx: 'auto' }}>
+        <Link
+          to="/centar-za-americke-studije-vudro-vilson"
+          sx={{
+            color: 'background',
+            fontSize: ['3rem', '5rem', null, '9rem'],
+            textTransform: 'uppercase',
+            transition: '0.3s',
+            '&:hover': {
+              color: 'primary'
+            }
+          }}
+        >
+          Srpsko-Američka akademija za lidere
+          </Link>
+      </h1>
+    </BgImage>
   );
 };
+
+const BgImage = styled(BackgroundImage)`
+  min-height: 60vh;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  opacity: 1 !important;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &,
+  &::before,
+  &::after {
+    background-attachment: fixed;
+  }
+  @media(min-width: 1200px) {
+    min-height: 87vh;
+  }
+  @media(min-width: 1600px) {
+    min-height: 100vh;
+  }
+`
