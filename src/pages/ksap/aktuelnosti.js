d91a198a978d67da211dcf621610e1bfeb57ae2b
Aleksandar Maksimovic <aleksandar.maksimovic@p3ds.net>
2020-04-12 23:00:05 +0200

    Create news page

---
+++ src/pages/ksap/aktuelnosti.js
@@ -0,0 +1,56 @@
+/** @jsx jsx */
+import styled from '@emotion/styled';
+import { jsx } from 'theme-ui';
+import { Banner } from '../../components/banner';
+import Hero from '../../components/hero';
+import Layout from '../../components/layout';
+import SEO from '../../components/seo';
+import { useHeroImage } from '../../hooks/useHeroImage';
+
+const Aktuelnosti = () => {
+  const { name, childImageSharp } = useHeroImage();
+
+  return (
+    <Layout>
+      <SEO title="Aktuelnosti" />
+      <Hero img={childImageSharp.fluid} alt={name} hero={true}>
+        <Banner>Aktuelnosti</Banner>
+      </Hero>
+      <Container>
+        <Content sx={{ fontFamily: 'body', color: 'primary' }}></Content>
+      </Container>
+    </Layout>
+  );
+};
+
+export default Aktuelnosti;
+
+const Container = styled.div`
+  @media (min-width: 1200px) {
+    max-width: 1140px;
+    margin: 0 auto;
+  }
+  @media (min-width: 1600px) {
+    max-width: 1410px;
+  }
+  @media (min-width: 1800px) {
+    max-width: 1420px;
+  }
+`;
+
+const Content = styled.div`
+  padding: 0 1.6rem;
+  letter-spacing: 2px;
+  font-size: 2rem;
+  line-height: 3rem;
+  font-weight: 300;
+  @media (min-width: 1200px) {
+    padding: 0;
+    font-size: 2rem;
+    line-height: 3.5rem;
+    text-align: left;
+  }
+  @media (min-width: 1800px) {
+    font-size: 2.5rem;
+  }
+`;
