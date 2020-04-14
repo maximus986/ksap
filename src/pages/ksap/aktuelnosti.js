051863a02759e0c1299f1516c229fd61ce7fd9e1
Aleksandar Maksimovic <aleksandar.maksimovic@p3ds.net>
2020-04-14 21:04:11 +0200

    Add content

--- src/pages/ksap/aktuelnosti.js
+++ src/pages/ksap/aktuelnosti.js
@@ -1,14 +1,78 @@
 /** @jsx jsx */
 import styled from '@emotion/styled';
-import { jsx } from 'theme-ui';
+import { jsx, useThemeUI } from 'theme-ui';
 import { Banner } from '../../components/banner';
 import Hero from '../../components/hero';
 import Layout from '../../components/layout';
 import SEO from '../../components/seo';
 import { useHeroImage } from '../../hooks/useHeroImage';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS, MARKS } from '@contentful/rich-text-types';
+import { SectionContainer } from '../../components/sectionContainer';
+import { graphql } from 'gatsby';
+import Image from 'gatsby-image';
 
-const Aktuelnosti = () => {
+export const PAGE_QUERY = graphql`
+  {
+    news: contentfulNews {
+      title
+      image {
+        fluid {
+          ...GatsbyContentfulFluid_withWebp
+        }
+      }
+      content {
+        json
+      }
+    }
+  }
+`;
+
+const Aktuelnosti = ({ data }) => {
+  const { news } = data;
   const { name, childImageSharp } = useHeroImage();
+
+  const {
+    theme: { colors },
+  } = useThemeUI();
+
+  const Bold = ({ children }) => (
+    <span sx={{ fontWeight: 'bold' }}>{children}</span>
+  );
+  const Text = ({ children }) => <p sx={{ marginBottom: 16 }}>{children}</p>;
+  const Heading5 = ({ children }) => (
+    <h5
+      sx={{
+        fontSize: ['2.5rem', '3rem'],
+        fontFamily: 'body',
+        mb: 16,
+        mt: 32,
+        textTransform: 'uppercase',
+      }}
+    >
+      {children}
+    </h5>
+  );
+  const UlList = ({ children }) => (
+    <ul sx={{ pl: ['4rem', '10rem'], textAlign: ['left'] }}>{children}</ul>
+  );
+  const OlList = ({ children }) => (
+    <ol sx={{ pl: ['4rem', '10rem'], textAlign: ['left'] }}>{children}</ol>
+  );
+  const ListItem = ({ children }) => <li sx={{}}>{children}</li>;
+
+  const options = {
+    renderMark: {
+      [MARKS.BOLD]: text => <Bold>{text}</Bold>,
+    },
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+      [BLOCKS.HEADING_5]: (node, children) => <Heading5>{children}</Heading5>,
+      [BLOCKS.UL_LIST]: (node, children) => <UlList>{children}</UlList>,
+      [BLOCKS.OL_LIST]: (node, children) => <OlList>{children}</OlList>,
+      [BLOCKS.LIST_ITEM]: (node, children) => <ListItem>{children}</ListItem>,
+    },
+  };
 
   return (
     <Layout>
@@ -16,17 +80,58 @@ const Aktuelnosti = () => {
       <Hero img={childImageSharp.fluid} alt={name} hero={true}>
         <Banner>Aktuelnosti</Banner>
       </Hero>
-      <Container>
-        <Content sx={{ fontFamily: 'body', color: 'primary' }}></Content>
-      </Container>
+      <SectionContainer sectionBgColor={colors.muted}>
+        <NewsContainer
+          sx={{
+            fontFamily: 'body',
+            textAlign: 'left',
+            a: {
+              fontSize: ['1.4rem', '2rem'],
+            },
+          }}
+        >
+          <figure
+            sx={{
+              maxWidth: ['400px', '600px'],
+              mx: ['auto'],
+              mb: ['3rem', null, '5rem'],
+              div: {
+                transition: 'link',
+              },
+              '&:hover': {
+                div: {
+                  transform: 'scale(1.2)',
+                  opacity: '0.8',
+                },
+              },
+            }}
+          >
+            <Image fluid={news.image.fluid} alt={news.title} />
+          </figure>
+          {documentToReactComponents(news.content.json, options)}
+        </NewsContainer>
+      </SectionContainer>
     </Layout>
   );
 };
 
 export default Aktuelnosti;
 
-const Container = styled.div`
+const NewsContainer = styled.div`
+  padding: 0 1.6rem;
+  letter-spacing: 2px;
+  font-size: 2rem;
+  line-height: 3rem;
+  font-weight: 300;
+  @media (min-width: 576px) {
+  }
+  @media (min-width: 768px) {
+    width: 100%;
+  }
   @media (min-width: 1200px) {
+    text-align: left;
+    font-size: 2rem;
+    line-height: 3.5rem;
     max-width: 1140px;
     margin: 0 auto;
   }
@@ -35,22 +140,6 @@ const Container = styled.div`
   }
   @media (min-width: 1800px) {
     max-width: 1420px;
-  }
-`;
-
-const Content = styled.div`
-  padding: 0 1.6rem;
-  letter-spacing: 2px;
-  font-size: 2rem;
-  line-height: 3rem;
-  font-weight: 300;
-  @media (min-width: 1200px) {
-    padding: 0;
-    font-size: 2rem;
-    line-height: 3.5rem;
-    text-align: left;
-  }
-  @media (min-width: 1800px) {
     font-size: 2.5rem;
   }
 `;
