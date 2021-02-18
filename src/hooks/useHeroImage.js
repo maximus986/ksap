c866b84ca500e7dc387abb1ebd164e9c1a7736b6
maximus986 <aleksandar.maksimovic2608@gmail.com>
2021-02-18 09:59:03 +0100

    Change hero image

--- src/hooks/useHeroImage.js
+++ src/hooks/useHeroImage.js
@@ -4,7 +4,7 @@ export const useHeroImage = () => {
   const { heroImage } = useStaticQuery(
     graphql`
       {
-        heroImage: file(relativePath: { eq: "hero.png" }) {
+        heroImage: file(relativePath: { eq: "ksap.jpg" }) {
           name
           childImageSharp {
             fluid(maxWidth: 4160, quality: 90) {
